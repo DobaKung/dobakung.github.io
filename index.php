fc958cfe5917c85ee32659314ec102d1b1075490
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-06-04 09:00:43 +0700

    Adds header.php
    Index gets header.php

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<? ob_start (); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Nathan Yiangsupapaanontr</title>
-    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-    <link rel="stylesheet" href="css/animations.css">
-    <link href="css/pavilion.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Karla:400' rel='stylesheet' type='text/css'>
-    <meta name="theme-color" content="#00399E">
-    <!--[if lt IE 9]>
-      <link rel="stylesheet" href="css/animations-ie-fix.css">
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+<?php include_once "header.php";?>
 <body>
     <div class="animatedParent">
         <nav class="animated fadeInDown">
